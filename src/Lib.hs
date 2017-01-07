bfd95867c8123aa69b88310a9514c7b4d5307268
Ricky Hariady <ricky.hariady@gmail.com>
2017-01-07 15:23:54 +0700

    add Environment, Appliance, and Device type update API

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,15 +11,31 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 
-data User = User
-  { userId        :: Int
-  , userFirstName :: String
-  , userLastName  :: String
+data Environment = Environment
+  { environmentId       :: Int
+  , environmentName     :: String
+  , environmentParent   :: Maybe Int
   } deriving (Eq, Show)
 
-$(deriveJSON defaultOptions ''User)
+$(deriveJSON defaultOptions ''Environment)
 
-type API = "users" :> Get '[JSON] [User]
+data Appliance = Appliance
+  { applianceId         :: Int
+  , applianceName       :: String
+  } deriving (Eq, Show)
+
+$(deriveJSON defaultOptions ''Appliance)
+
+data Device = Device
+  { deviceId    :: Int
+  , deviceType  :: String
+  } deriving (Eq, Show)
+
+$(deriveJSON defaultOptions ''Device)
+
+type FisbangAPI = "environments" :> Get '[JSON] [Environment]
+             :<|> "appliances" :> Get '[JSON] [Appliance]
+             :<|> "devices" :> Get '[JSON] [Device]
 
 startApp :: IO ()
 startApp = run 8080 app
@@ -27,13 +43,31 @@ startApp = run 8080 app
 app :: Application
 app = serve api server
 
-api :: Proxy API
+api :: Proxy FisbangAPI
 api = Proxy
 
-server :: Server API
-server = return users
+server :: Server FisbangAPI
+server = return environments
+    :<|> return appliances
+    :<|> return devices
 
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
+environments :: [Environment]
+environments = [ Environment 1 "My Home" Nothing
+               , Environment 2 "Living Room" $ Just 1
+               , Environment 3 "Bed Room" $ Just 1
+               ]
+
+appliances :: [Appliance]
+appliances = [ Appliance 1 "Television"
+             , Appliance 2 "Refrigerator"
+             , Appliance 3 "Air Conditioner"
+             ]
+
+devices :: [Device]
+devices = [ Device 1 "Main Power"
+          , Device 2 "Socket"
+          , Device 3 "Socket"
+          ]
+
+getEnvironments :: [Environment]
+getEnvironments = 
